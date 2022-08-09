d73228cc9a857475a0433fa576339fb3f87795ac
o_omi <whdudal1217@naver.com>
2022-08-09 11:09:00 +0900

    :sparkles:   create emailCheckTokenGeneratedAt & canSendConfirmEmail

--- src/main/java/com/studyolle/domain/Account.java
+++ src/main/java/com/studyolle/domain/Account.java
@@ -38,6 +38,8 @@ public class Account {
 
   private String emailCheckToken;
 
+  private LocalDateTime emailCheckTokenGeneratedAt;
+
   private LocalDateTime joinedAt;
 
   private String bio;
@@ -65,6 +67,7 @@ public class Account {
 
   public void generateEmailCheckToken() {
     this.emailCheckToken = UUID.randomUUID().toString();
+    this.emailCheckTokenGeneratedAt = LocalDateTime.now();
   }
 
   public void completeSignUp() {
@@ -75,4 +78,8 @@ public class Account {
   public boolean isValidToken(String token) {
     return this.emailCheckToken.equals(token);
   }
+
+    public boolean canSendConfirmEmail() {
+      return this.emailCheckTokenGeneratedAt.isBefore(LocalDateTime.now().minusHours(1));
+    }
 }
