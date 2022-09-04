c743d84e21835efd1dbf71efcb878300897295cc
o_omi <whdudal1217@naver.com>
2022-09-04 21:05:59 +0900

    :recycle: remove annotations

--- src/main/java/com/studyolle/account/AccountController.java
+++ src/main/java/com/studyolle/account/AccountController.java
@@ -34,32 +34,15 @@ public class AccountController {
 
   @GetMapping("/sign-up")
   public String signUpForm(Model model){
-    // model에 attributeName은 생략이 가능 합니다.
-    //model.addAttribute("signUpForm",new SignUpForm());
     model.addAttribute(new SignUpForm());
-
     return "account/sign-up";
   }
 
   @PostMapping("/sign-up")
   public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors){
-    //@ModelAttribute : 복합객체를 받아야 할 때 사용하나 생략 가능
-    //Errors : 데이터를 컨버젼하거나 바인딩 할 때 에러를 받습니다.
-    // ( SignUpForm에서 Hibernate로 검증하는데 이에 통과하지 못하면 errors를 뱉을 수 있음)
-    //errors에 에러가 있다면 form html을 다시 출력하면 됩니다!
     if(errors.hasErrors()){
       return "account/sign-up";
     }
-
-    /*
-    이런식으로 유효성 검증하는 것이 번거롭다면 InitBinder 사용!
-    InitBinder 사용하면...
-    PostMapping으로 요청이 오고 그 요청에 담긴 객체의 유효성을 검증 할 수 있습니다!
-    signUpFormValidator.validate(signUpForm, errors);
-    if(errors.hasErrors()){
-      return "account/sign-up";
-    }
-     */
     Account loginAccount = accountService.processNewAccount(signUpForm);
     accountService.login(loginAccount);
 
