66c0348b87daaf89b908202bd39d46e0df864299
o_omi <whdudal1217@naver.com>
2022-09-06 18:40:58 +0900

    :sparkles: Study opening and show study members function

---
+++ src/main/java/com/studyolle/domain/Study.java
@@ -0,0 +1,98 @@
+package com.studyolle.domain;
+
+import com.studyolle.account.UserAccount;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedAttributeNode;
+import javax.persistence.NamedEntityGraph;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Type;
+
+@NamedEntityGraph(name = "Study.withAll", attributeNodes = {
+    @NamedAttributeNode("tags")
+    ,@NamedAttributeNode("zones")
+    ,@NamedAttributeNode("managers")
+    ,@NamedAttributeNode("members")
+})
+@Entity
+@Getter @Setter @EqualsAndHashCode(of = "id")
+@Builder @AllArgsConstructor @NoArgsConstructor
+public class Study {
+    @Id @GeneratedValue
+    private Long id;
+
+    @ManyToMany
+    private Set<Account> managers = new HashSet<>();
+
+    @ManyToMany
+    private Set<Account> members = new HashSet<>();
+
+    @Column(unique = true)
+    private String path;
+
+    private String title;
+
+    private String shortDescription;
+
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String fullDescription;
+
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String image;
+
+    @ManyToMany
+    private Set<Tag> tags = new HashSet<>();
+
+    @ManyToMany
+    private Set<Zone> zones = new HashSet<>();
+
+    private LocalDateTime publishedDateTime;
+
+    private LocalDateTime closedDateTime;
+
+    private LocalDateTime recruitingUpdatedDateTime;
+
+    private boolean recruiting;
+
+    private boolean published;
+
+    private boolean closed;
+
+    private boolean useBanner;
+
+    public void addManager(Account account) {
+        this.managers.add(account);
+    }
+
+    public void addMember(Account account) {
+        this.members.add(account);
+    }
+
+    public boolean isMember(UserAccount userAccount){
+        return this.members.contains(userAccount.getAccount());
+    }
+
+    public boolean isManager(UserAccount userAccount){
+        return this.managers.contains(userAccount.getAccount());
+    }
+
+    public boolean isJoinable(UserAccount userAccount){
+        Account account = userAccount.getAccount();
+        return this.isPublished() && this.isRecruiting() && !this.members.contains(account) && !this.managers.contains(account);
+    }
+
+}
---
+++ src/main/java/com/studyolle/study/StudyController.java
@@ -0,0 +1,68 @@
+package com.studyolle.study;
+
+import com.studyolle.account.CurrentUser;
+import com.studyolle.domain.Account;
+import com.studyolle.domain.Study;
+import com.studyolle.study.form.StudyForm;
+import com.studyolle.study.validator.StudyFormValidator;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class StudyController {
+
+    private final StudyService studyService;
+    private final ModelMapper modelMapper;
+    private final StudyFormValidator studyFormValidator;
+    private final StudyRepository studyRepository;
+
+    @InitBinder("studyForm")
+    public void initBinder(WebDataBinder webDataBinder){
+        webDataBinder.addValidators(studyFormValidator);
+    }
+
+    @GetMapping("/new-study")
+    public String newStudyForm(@CurrentUser Account account, Model model){
+        model.addAttribute(account);
+        model.addAttribute(new StudyForm());
+        return "study/form";
+    }
+
+    @PostMapping("/new-study")
+    public String newStudySubmit(@CurrentUser Account account, @Valid StudyForm studyForm, Errors errors, Model model){
+        if(errors.hasErrors()){
+            model.addAttribute(account);
+            return "study/form";
+        }
+
+        Study newStudy = studyService.createNewStudy(modelMapper.map(studyForm, Study.class), account);
+        return "redirect:/study/" + URLEncoder.encode(newStudy.getPath(), StandardCharsets.UTF_8);
+    }
+
+    @GetMapping("/study/{path}")
+    public String viewStudy(@CurrentUser Account account, @PathVariable String path, Model model){
+        model.addAttribute(account);
+        model.addAttribute(studyRepository.findByPath(path));
+        return "study/view";
+    }
+
+    @GetMapping("/study/{path}/members")
+    public String viewStudyMember(@CurrentUser Account account, @PathVariable String path, Model model){
+        model.addAttribute(account);
+        model.addAttribute(studyRepository.findByPath(path));
+        return "study/members";
+    }
+
+}
